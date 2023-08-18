cd8b0f52bf7b2eeed0f20ba42b99908af8670684
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-18 21:21:58 +0900

    Chore / 초기파일 셋팅(react-query 누락 설치, App.jsx확장자 변경, styled-reset, GlobalStyle 연결)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-import Router from "./shared/Router";
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Router from './shared/Router';
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />;
+      <Router />
     </QueryClientProvider>
   );
 }
