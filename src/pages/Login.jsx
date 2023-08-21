b16467a61cf4fb117e3d81250b92acd6a9b0ac72
sua <choisua98@gmail.com>
2023-08-21 15:31:37 +0900

    Feat/로그인기능구현#5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../firebase/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //로그인 버튼 클릭 핸들러
+  const onLoginButtonClickHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       <div>login page</div>
       <form>
-        <div>이메일영역</div>
-        <div>비밀번호영역</div>
-        <button>로그인</button>
+        <div>
+          <input
+            type="email"
+            value={email}
+            name="email"
+            placeholder="이메일"
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          ></input>
+        </div>
+        <div>
+          <input
+            type="password"
+            value={password}
+            name="password"
+            placeholder="비밀번호"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+        </div>
+        <button type="submit" onClick={onLoginButtonClickHandler}>
+          로그인
+        </button>
       </form>
     </div>
   );
