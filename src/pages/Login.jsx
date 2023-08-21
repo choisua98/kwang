b1132f19b94c44deb1355714efcd026f02be4a4e
sua <choisua98@gmail.com>
2023-08-21 19:26:39 +0900

    Feat/네이버로그인구현중 에러-튜터님과 해결중#5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from '../firebase/firebaseConfig';
 import {
   signInWithEmailAndPassword,
@@ -8,12 +8,28 @@ import {
 } from 'firebase/auth';
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const provider1 = new GoogleAuthProvider();
   const auth = getAuth();
 
-  //구글 로그인 버튼 클릭 핸들러
+  // --이메일 로그인--
+  const onLoginButtonClickHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      console.log(userCredential);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // --구글 로그인--
   const onGoogleLoginButtonClickHandler = (e) => {
     e.preventDefault();
     signInWithPopup(auth, provider1)
@@ -38,20 +54,43 @@ const Login = () => {
         // ...
       });
   };
+  // ---네이버 로그인---
+  const { naver } = window;
 
-  //로그인 버튼 클릭 핸들러
-  const onLoginButtonClickHandler = async (e) => {
-    e.preventDefault();
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password,
-      );
-      console.log(userCredential);
-    } catch (error) {
-      console.error(error);
-    }
+  const naverLogin = new naver.LoginWithNaverId({
+    clientId: 'hsnzexHuuJiVHO_hh5EP',
+    // process.env.REACT_APP_NAVER_CLIENT_ID,
+    callbackUrl: 'http://www.localhost:3000/login',
+    isPopup: true,
+    loginButton: {
+      color: 'green',
+      type: 1,
+      height: 50,
+    },
+  });
+
+  useEffect(() => {
+    naverLogin.init(); //초기화
+    console.log('init!');
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    await naverLogin.getLoginStatus((status) => {
+      console.log(`로그인?: ${status}`);
+      if (status) {
+        setUser({ ...naverLogin.user });
+        if (window.opener && window.opener.location) {
+          window.opener.location.href = 'http://localhost:3000/login';
+          window.close();
+        }
+      }
+    });
+  };
+
+  const naverLogout = () => {
+    localStorage.removeItem('com.naver.nid.access_token');
+    window.location.reload();
   };
   return (
     <div>
@@ -86,7 +125,27 @@ const Login = () => {
         </button>
         <br />
         <button onClick={onGoogleLoginButtonClickHandler}>google</button>
+        <div className="grid-naver" id="naverIdLogin"></div>
       </form>
+      <div>
+        {user ? (
+          <div>
+            <h2>네이버 로그인 성공!</h2>
+            <h3>사용자 이름</h3>
+            <div>{user.name}</div>
+            <h3>사용자 이메일</h3>
+            <div>{user.email}</div>
+            <h3>사용자 프로필사진</h3>
+            <img src={user.profile_image} alt="프로필 사진" />
+            <button onClick={naverLogout}>로그아웃</button>
+          </div>
+        ) : (
+          // 네이버 로그인 버튼
+          <div>
+            <div id="naverIdLogin"></div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
