4d71f5306b95cac9b3f6e6ff379ecab7206eaf70
sua <choisua98@gmail.com>
2023-08-21 14:59:13 +0900

    Feat/회원가입기능구현#5

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div>login page</div>
+      <form>
+        <div>이메일영역</div>
+        <div>비밀번호영역</div>
+        <button>로그인</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
