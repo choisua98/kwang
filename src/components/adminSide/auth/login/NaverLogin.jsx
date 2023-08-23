4d4776809a57ba1234ae2b825c9d145cfc652f7d
sua <choisua98@gmail.com>
2023-08-23 12:08:25 +0900

    Feat/네이버로그인유저정보를 이용해서 파이어베이스 이메일패스워드회원가입시키기or로그인 구현완료.#5

--- src/components/adminSide/auth/login/NaverLogin.jsx
+++ src/components/adminSide/auth/login/NaverLogin.jsx
@@ -1,24 +1,16 @@
 import React, { useEffect } from 'react';
-import { getAuth } from 'firebase/auth';
-// import firebase from 'firebase/app';
-// import 'firebase/firestore';
-import { addDoc, collection } from 'firebase/firestore';
-
+import {
+  getAuth,
+  fetchSignInMethodsForEmail,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useAtom } from 'jotai';
 import { userAtom } from '../../../../atoms/Atom';
-import { db } from '../../../../firebase/firebaseConfig';
+
 const NaverLogin = () => {
   const [user, setUser] = useAtom(userAtom);
   const auth = getAuth();
-
-  // --파이어베이스에 데이터 저장--
-  const saveUserData = async (userData) => {
-    const collectionRef = collection(db, 'users');
-    await addDoc(collectionRef, userData);
-    // const db = firebase.firestore();
-    // const usersCollection = db.collection('users');
-    // return usersCollection.add(userData);
-  };
 
   // --네이버 로그인--
   const { naver } = window;
@@ -38,33 +30,53 @@ const NaverLogin = () => {
     naverLogin.getLoginStatus(async (status) => {
       if (status) {
         console.log(`로그인?: ${status}`);
+        console.log(naverLogin.user);
         setUser(naverLogin.user);
+        //네이버로그인 유저정보를 이용해서 firebase 이메일+패스워드로 가입시켜주기.
+        //firebase에 등록된 기존회원이면 로그인 -> 안되면 비회원이니까 회원가입시켜주기.
+        const checkEmailExists = async () => {
+          try {
+            const methods = await fetchSignInMethodsForEmail(
+              auth,
+              naverLogin.user.email,
+            );
+            if (methods.length > 0) {
+              console.log('기존회원/로그인진행합니다.');
+              try {
+                const userCredential = await signInWithEmailAndPassword(
+                  auth,
+                  naverLogin.user.email,
+                  naverLogin.user.id,
+                );
+                console.log(userCredential);
+                console.log('로그인성공.');
+              } catch (error) {
+                console.error(error);
+              }
+            } else {
+              console.log('비회원/회원가입진행합니다.');
+              createUserWithEmailAndPassword(
+                auth,
+                naverLogin.user.email,
+                naverLogin.user.id,
+              )
+                .then((userCredential) => {
+                  console.log(`회원가입유저${userCredential}`);
+                  console.log('회원가입성공. 로그인완료.');
+                })
+                .catch((error) => {
+                  console.error(error);
+                });
+            }
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        checkEmailExists();
       }
     });
   };
   console.log(user);
-  //8/22 데이터베이스에 넣는 로직 진행중. 네이버 로그인버튼을 누를때 한번만 데이터베이스에 들어가게 해야함. 지금은 유즈이팩트로 해도 몇번씩 들어감..
-
-  const naverUserData = {
-    age: `${user?.age}`,
-    birthday: `${user?.birthday}`,
-    email: `${user?.email}`,
-    gender: `${user?.gender}`,
-    id: `${user?.id}`,
-    mobile: `${user?.mobile}`,
-    name: `${user?.name}`,
-    nickname: `${user?.nickname}`,
-    profile_image: `${user?.profile_image}`,
-  };
-  useEffect(() => {
-    saveUserData(naverUserData)
-      .then((docRef) => {
-        console.log('Document written with ID:', docRef.id);
-      })
-      .catch((error) => {
-        console.error('Error adding document:', error);
-      });
-  }, [saveUserData]);
 
   useEffect(() => {
     initializeNaverLogin();
