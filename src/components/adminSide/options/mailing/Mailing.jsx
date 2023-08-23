e074e4b627da5e1dbada02e22781b26bfbf85d78
YongA <ji5919@naver.com>
2023-08-23 14:55:03 +0900

    Temporary commit

--- src/components/adminSide/options/mailing/Mailing.jsx
+++ src/components/adminSide/options/mailing/Mailing.jsx
@@ -1,32 +1,51 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { M } from './Mailing.styles';
 import useInput from '../../../../hooks/useInput';
-import {
-  addDoc,
-  collection,
-  doc,
-  getDoc,
-  serverTimestamp,
-} from 'firebase/firestore';
+import { addDoc, collection, doc, getDoc } from 'firebase/firestore';
 import { auth, db } from '../../../../firebase/firebaseConfig';
 
 const Mailing = () => {
+  const navigate = useNavigate();
   const [toggle, setToggle] = useState(true);
   const [title, handleTitleChange] = useInput();
-  const [content, handleContentChange] = useInput();
+  const [description, handleDescriptionChange] = useInput();
 
   const handleAddButtonClick = async (e) => {
     e.preventDefault();
 
-    // Firestore에 데이터 추가
-    await addDoc(collection(db, 'template'), {
-      title,
-      content,
-      optionKind: 'mailing',
-      createdAt: serverTimestamp(),
-      // userId: userDocSnapshot.id,
-    });
-    console.log('데이터가 추가되었습니다.');
+    // 사용자 UID 가져오기
+    const userUid = auth.currentUser?.uid;
+
+    if (!userUid) {
+      alert('사용자가 로그인하지 않았습니다.');
+      navigate('/login');
+      return;
+    }
+
+    // 사용자 UID를 이용하여 문서 경로 만들기
+    const userDocPath = `users/${userUid}`;
+
+    // 사용자 문서 조회
+    const userDocRef = doc(db, userDocPath);
+    const userDocSnapshot = await getDoc(userDocRef);
+
+    if (userDocSnapshot.exists()) {
+      const userData = userDocSnapshot.data();
+      console.log('사용자 데이터:', userData);
+
+      // Firestore에 데이터 추가
+      await addDoc(collection(db, 'template'), {
+        title,
+        description,
+        blockKind: 'mailing',
+        // createdAt: serverTimestamp(),
+        userId: userDocSnapshot.id,
+      });
+      console.log('데이터가 추가되었습니다.');
+    } else {
+      console.log('사용자 데이터가 없습니다');
+    }
   };
 
   const swtichToggle = () => {
@@ -52,8 +71,8 @@ const Mailing = () => {
             name="content"
             type="text"
             placeholder="설명을 작성해 주세요"
-            value={content}
-            onChange={handleContentChange}
+            value={description}
+            onChange={handleDescriptionChange}
           />
           <button type="submit">저장하기</button>
         </M.ContentForm>
