beab68df63d58667a036f846a3bbd5d331dce88a
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-27 17:31:22 +0900

    Feat / 공통 :  Pretendard 폰트 적용, 메뉴 토글 추가, 로그인 클릭시 페이지 이동 홈 : swiper 적용 및 작업 Link : 이미지 높이가 길 때 고정값 추가 #37

---
+++ src/components/customerSide/Home/MainBanner.styles.js
@@ -0,0 +1,9 @@
+import { styled } from 'styled-components';
+
+export const M = {
+  Container: styled.div``,
+  // Image: styled.img`
+  //   width: 100%;
+  //   height: auto;
+  // `,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import UserHome from '../components/customerSide/Home/UserHome';
+// import MainBanner from '../components/customerSide/Home/MainBanner';
+// import Service from '../components/customerSide/Home/Service';
+// import BestCreator from '../components/customerSide/Home/BestCreator';
 
 const Home = () => {
-  return <div></div>;
+  return (
+    // <div style={{ padding: '0 20px' }}>
+    //   <MainBanner />
+    //   <Service />
+    //   <BestCreator />
+    // </div>
+    <>
+      <UserHome />
+    </>
+  );
 };
 
 export default Home;
