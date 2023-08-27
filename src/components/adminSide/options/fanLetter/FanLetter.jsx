e32432e53f4c17da362fe308828000f0387ae058
heejin_song <song_heejin@naver.com>
2023-08-27 19:06:34 +0900

    feat / fanletter 새로고침시 인풋 값 사라지는 문제 해결 #38

--- src/components/adminSide/options/fanLetter/FanLetter.jsx
+++ src/components/adminSide/options/fanLetter/FanLetter.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { F } from './FanLetter.styles';
 import {
+  QuerySnapshot,
   addDoc,
   collection,
   doc,
+  getDoc,
+  getDocs,
+  query,
   serverTimestamp,
   updateDoc,
+  where,
 } from 'firebase/firestore';
 import { auth, db } from '../../../../firebase/firebaseConfig';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -21,13 +26,12 @@ const FanLetter = () => {
 
   // Jotai에서 유저 정보 가져오기
   const user = useAtomValue(userAtom);
-  console.log('1', user);
 
   // 유저의 UID 가져오기
   const userUid = auth.currentUser?.uid;
 
   const blockId = location.state ? location.state.blocksId : null;
-  const [blocks] = useAtom(blocksAtom);
+  const [blocks, setBlocks] = useAtom(blocksAtom);
   const selectedBlock = blocks.find((block) => block.id === blockId) || '';
 
   const addButtonClick = async (e) => {
@@ -61,6 +65,39 @@ const FanLetter = () => {
     });
     navigate('/admin');
   };
+
+  // firebase에서 데이터 불러오기
+  const fetchData = async () => {
+    try {
+      // 쿼리 실행하여 데이터 가져오기
+      const q = query(
+        collection(db, 'template'),
+        where('userId', '==', userUid),
+      );
+      const querySnapshot = await getDocs(q);
+
+      // 가져온 데이터를 가공하여 배열에 저장
+      const initialDocuments = [];
+      querySnapshot.forEach((doc) => {
+        const data = {
+          id: doc.id,
+          ...doc.data(),
+        };
+        initialDocuments.push(data);
+      });
+
+      // 가공된 데이터를 상태에 업데이트
+      setBlocks(initialDocuments);
+    } catch (error) {
+      console.error('데이터 가져오기 오류:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (user) {
+      fetchData();
+    }
+  }, [user]);
 
   return (
     <F.Container onSubmit={blockId ? editButtonClick : addButtonClick}>
