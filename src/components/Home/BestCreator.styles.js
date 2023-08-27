e8d381ae5c7fa776163eca42cf104e05423b8253
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-27 23:55:12 +0900

    Rename / Home.jsx 관련 폴더 구조 변경 #37

---
+++ src/components/Home/BestCreator.styles.js
@@ -0,0 +1,5 @@
+import { styled } from 'styled-components';
+
+export const B = {
+  Container: styled.div``,
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import UserHome from '../components/customerSide/Home/UserHome';
-// import MainBanner from '../components/customerSide/Home/MainBanner';
-// import Service from '../components/customerSide/Home/Service';
-// import BestCreator from '../components/customerSide/Home/BestCreator';
+import MainBanner from '../components/Home/MainBanner';
+import Service from '../components/Home/Service';
+import BestCreator from '../components/Home/BestCreator';
 
 const Home = () => {
   return (
-    // <div style={{ padding: '0 20px' }}>
-    //   <MainBanner />
-    //   <Service />
-    //   <BestCreator />
-    // </div>
-    <>
-      <UserHome />
-    </>
+    <div style={{ padding: '0 20px' }}>
+      <MainBanner />
+      <Service />
+      <BestCreator />
+    </div>
   );
 };
 
