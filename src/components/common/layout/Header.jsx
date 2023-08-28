5276ba9cd78d979a5ed6e7feb81cdeb679775b31
sua <choisua98@gmail.com>
2023-08-28 17:50:49 +0900

    Feat/pages Signup.jsx 에 작업했던 내용을 component에EmailSignup.jsx 생성해서 붙여넣음.테스트가 용이하도록 Header에 로그아웃기능을 넣었음. 로그아웃시 로그인페이지로 이동함. 회원가입시 이메일 중복확인 기능구현, 회원가입 유효성 검사 완료#5

--- src/components/common/layout/Header.jsx
+++ src/components/common/layout/Header.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Row, Col, Button } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 import { ReactComponent as Logo } from '../../../assets/images/logo.svg';
 import { H } from './Header.styles';
+import { useAtom } from 'jotai';
+import { userAtom } from '../../../atoms/Atom';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../../firebase/firebaseConfig';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const [user, setUser] = useAtom(userAtom);
   // 메뉴 열림
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   // 메뉴 열렸을 때 바깥 영역 클릭시 닫힘
@@ -14,6 +20,16 @@ const Header = () => {
   // 상단 메뉴 클릭시 메뉴 열기/닫기 토글
   const handleMenuClick = () => {
     setIsMenuOpen(!isMenuOpen);
+  };
+  // 로그아웃 버튼클릭 핸들러 -> 로그아웃 시 로그인 페이지로 이동시켜 놓음
+  const onLogoutButtonClickHandler = async () => {
+    if (user) {
+      console.log('유저가있습니다. 로그아웃하겠습니다.');
+    }
+    await signOut(auth); //파이어베이스 로그아웃
+    setUser('');
+    alert('로그아웃 되었습니다.');
+    navigate('/login');
   };
 
   // 마운트 시 외부 영역 클릭시 이벤트 추가, 언마운트 시 제거
@@ -60,7 +76,7 @@ const Header = () => {
               <Link to="/login">로그인</Link>
             </li>
             <li>
-              <Link to="#">로그아웃</Link>
+              <button onClick={onLogoutButtonClickHandler}>로그아웃</button>
             </li>
           </ul>
         </H.MenuContentWrapper>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,73 +1,10 @@
-import React, { useState } from 'react';
-import { auth } from '../firebase/firebaseConfig';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import EmailSignup from '../components/adminSide/auth/signup/EmailSignup';
 
 const Signup = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const navigate = useNavigate();
-
-  // 회원가입 버튼클릭 핸들러
-  const onSignupButtonClickHandler = async (e) => {
-    e.preventDefault();
-    await createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // 회원가입 성공시
-        console.log(userCredential);
-      })
-      .catch((error) => {
-        // 회원가입 실패시
-        console.error(error);
-      });
-    navigate('/admin');
-  };
-
   return (
     <div>
-      <div>signup page</div>
-      <form>
-        <div>
-          <input
-            type="email"
-            value={email}
-            name="email"
-            placeholder="이메일"
-            required
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
-          ></input>
-        </div>
-        <div>
-          <input
-            type="password"
-            value={password}
-            name="password"
-            placeholder="비밀번호"
-            required
-            onChange={(e) => {
-              setPassword(e.target.value);
-            }}
-          ></input>
-        </div>
-        <div>
-          <input
-            type="password"
-            value={confirmPassword}
-            name="confirmPassword"
-            placeholder="비밀번호 확인"
-            required
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-            }}
-          ></input>
-        </div>
-        <button type="submit" onClick={onSignupButtonClickHandler}>
-          회원가입
-        </button>
-      </form>
+      <EmailSignup />
     </div>
   );
 };
