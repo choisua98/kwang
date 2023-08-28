3e9aac06d33a26a1ccf01d4d9d80eb67b6a7d76b
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-28 11:28:42 +0900

    Revert "Merge pull request #49 from choisua98/37-feat-home"
    This reverts commit 6e644d31ad6c9231e62b8f9c80c74ed48566d289, reversing
    changes made to b220466a342f32776ad9306646f7748ae08f23c7.

---
+++ src/components/customerSide/Home/MainBanner.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import { styled } from 'styled-components';
+import { M } from './MainBanner.styles';
+import { ReactComponent as PromoBanner } from '../../../assets/images/customer/home/banner/promo/promo-banner-1.svg';
+
+const MainBanner = () => {
+  return (
+    <>
+      <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        // navigation
+        // pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+        style={{ margin: '20px auto 0', width: '349px', height: '100px' }}
+      >
+        <SwiperSlide>
+          <PromoBanner alt="Promo Banner 1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <PromoBanner alt="Promo Banner 1" />
+        </SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
+
+      {/* <Swiper
+        // install Swiper modules
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper> */}
+    </>
+  );
+};
+
+export default MainBanner;
