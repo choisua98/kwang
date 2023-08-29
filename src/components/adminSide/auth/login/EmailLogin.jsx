9b8a59067545548ecbe05b8f2142ff064d5916d3
sua <choisua98@gmail.com>
2023-08-29 11:28:00 +0900

    Fix/헤더에 로그아웃버튼 클릭시'/'로 아동, 로그아웃 안되는 현상 리로드로 해결,로그인유효성 검사 완료, 네이버랑 카카로 로그인 이메일이 동일할 경우 에러해결#5

--- src/components/adminSide/auth/login/EmailLogin.jsx
+++ src/components/adminSide/auth/login/EmailLogin.jsx
@@ -12,19 +12,45 @@ const EmailLogin = () => {
   // --이메일 로그인 버튼클릭 핸들러--
   const onLoginButtonClickHandler = async (e) => {
     e.preventDefault();
-
-    await signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // 로그인 성공시
-        console.log(userCredential);
-      })
-      .catch((error) => {
-        // 로그인 실패시
-        console.error(error);
-      });
-    navigate('/admin');
+    try {
+      if (!email) {
+        alert('이메일을 입력해주세요.');
+        return;
+      }
+      if (!password) {
+        alert('비밀번호를 입력해주세요.');
+        return;
+      }
+      await signInWithEmailAndPassword(auth, email, password).then(
+        (userCredential) => {
+          // 로그인 성공시
+          console.log(userCredential);
+          navigate('/admin');
+        },
+      );
+    } catch (error) {
+      console.log(error.code);
+      alert(getErrorMessage(error.code));
+    }
   };
 
+  // 에러 코드에 따른 유효성 검사
+  const getErrorMessage = (errorCode) => {
+    switch (errorCode) {
+      case 'auth/user-not-found':
+        return '가입되지 않은 이메일입니다.';
+      case 'auth/wrong-password':
+        return '잘못된 비밀번호입니다.';
+      case 'auth/network-request-failed':
+        return '네트워크 연결에 실패 하였습니다.';
+      case 'auth/invalid-email':
+        return '잘못된 이메일 형식입니다.';
+      case 'auth/internal-error':
+        return '잘못된 요청입니다.';
+      default:
+        return '로그인에 실패하셨습니다.';
+    }
+  };
   return (
     <div>
       <div>login page</div>
