41f47f954ecf00c103db194b50edea7348f122e2
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-30 21:42:34 +0900

    Fix / 속도개선, Home.jsx 테마 임시 제거 #4

---
+++ src/pages/Home.styles.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const H = {
+  BodyWrapper: styled.div`
+    overflow: hidden;
+    padding: 0 20px;
+    width: 100%;
+    height: 100vh;
+    box-sizing: border-box;
+    background: #fff;
+  `,
+};
