4d02108e0ffa6a8887f5db3f4fef5179b5cbd837
Hyung-Jung Son <born2ocode@gmail.com>
2023-08-31 12:57:41 +0900

    Feat / 팬레터 기능 구현 완료 #79

--- src/components/customerSide/fanletterService/FanletterService.jsx
+++ src/components/customerSide/fanletterService/FanletterService.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { F } from './FanletterService.styles';
-import { collection, getDocs, query, where } from 'firebase/firestore';
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  serverTimestamp,
+  where,
+} from 'firebase/firestore';
 import { auth, db } from '../../../firebase/firebaseConfig';
 import { Button, Col, Input, Modal, Row } from 'antd';
 import { useAtom } from 'jotai';
 import { modalVisibleAtom } from '../../../atoms/Atom';
 const { TextArea } = Input;
 
-const onChange = (e) => {
-  console.log('Change:', e.target.value);
-};
-
 const FanletterService = () => {
-  // 사용자 UID 가져오기
-  const userUid = auth.currentUser?.uid;
-  const [modalVisible, setModalVisible] = useAtom(modalVisibleAtom);
-  const [templates, setTemplates] = useState([]);
+  const userUid = auth.currentUser?.uid; // 사용자 UID 가져오기
+  const [modalVisible, setModalVisible] = useAtom(modalVisibleAtom); // 모달
+  const [templates, setTemplates] = useState([]); // 템플릿 데이터 저장
+  const [description, setDescription] = useState(''); // TextArea 값을 저장
 
   useEffect(() => {
     const fetchTemplates = async () => {
@@ -35,44 +38,65 @@ const FanletterService = () => {
     fetchTemplates();
   }, [userUid]);
 
+  // 파이어스토어에 데이터 전송
+  const submitButtonClick = async () => {
+    try {
+      await addDoc(collection(db, 'userTemplate'), {
+        dataKind: 'fanletterData',
+        description,
+        createdAt: serverTimestamp(),
+        userId: userUid,
+      });
+      setModalVisible(true);
+    } catch (error) {
+      console.error('팬레터 데이터 추가 오류: ', error);
+    }
+  };
+
   return (
-    <>
-      <Row justify="center">
+    <div style={{ padding: '20px' }}>
+      <Row justify="left">
+        <h1>팬레터 보내기</h1>
         {templates.map((template) => {
           return (
-            <div key={template.id}>
-              <h1>{template.title}</h1>
+            <div key={template.id} style={{ margin: '10px auto 0' }}>
               <h2>{template.description}</h2>
             </div>
           );
         })}
       </Row>
-      <Row justify="center">
-        <h3>
-          TO. 크왕에게<span>11/100자</span>
+      <Row justify="left">
+        <h3 style={{ display: 'flex', width: '100%' }}>
+          TO. 크왕에게
+          <span style={{ marginLeft: 'auto' }}>{description.length}/100자</span>
         </h3>
         <TextArea
+          id="description"
+          name="description"
+          value={description}
           showCount
           maxLength={100}
           style={{
+            margin: '10px auto 0',
             height: 120,
             marginBottom: 24,
           }}
-          onChange={onChange}
-          placeholder="can resize"
+          onChange={(e) => setDescription(e.target.value)}
+          placeholder="설명을 작성해 주세요"
         />
       </Row>
-
       <Row justify="center">
         <Button
           type="primary"
-          onClick={() => setModalVisible(true)}
-          style={{ width: '100%' }}
+          onClick={() => {
+            submitButtonClick();
+            setModalVisible(true);
+          }}
+          style={{ margin: '20px auto 0', width: '100%' }}
         >
           발송하기
         </Button>
       </Row>
-
       <Modal
         title=""
         centered
@@ -97,7 +121,7 @@ const FanletterService = () => {
           </Col>
         </Row>
       </Modal>
-    </>
+    </div>
   );
 };
 
