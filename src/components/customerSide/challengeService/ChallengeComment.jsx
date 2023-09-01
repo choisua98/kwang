be2210d5746e45cdc1c0d56f8dadd5054ebb7601
leesoyul <tndus1303@gmail.com>
2023-09-02 02:50:47 +0900

    Fix / 댓글 count 값 새로고침 시 초기화 되는 문제, 파이어스토어에 count 값 업데이트 후 불러오기로 해결 #100

--- src/components/customerSide/challengeService/ChallengeComment.jsx
+++ src/components/customerSide/challengeService/ChallengeComment.jsx
@@ -2,16 +2,17 @@ import {
   collection,
   deleteDoc,
   doc,
+  getDoc,
   getDocs,
   setDoc,
   updateDoc,
 } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { db } from '../../../firebase/firebaseConfig';
-// import { useParams } from 'react-router-dom';
 import { styled } from 'styled-components';
 import { useAtom } from 'jotai';
 import { countAtom } from '../../../atoms/Atom';
+import { nanoid } from 'nanoid';
 
 const ChallengeComment = () => {
   const [nickname, setNickname] = useState('');
@@ -19,8 +20,6 @@ const ChallengeComment = () => {
   const [comment, setComment] = useState('');
   const [comments, setComments] = useState([]); // 댓글 데이터 저장
   const [count, setCount] = useAtom(countAtom); // 댓글 추가될 때 마다 카운트 + 1
-  //   const [editMode, setEditMode] = useState(null); // 수정 input 창과 수정하기 버튼 활성화
-  //   const [updateComment, setUpdateComment] = useState(''); // 수정된 댓글
 
   // 댓글 데이터를 가져오는 함수
   const fetchComments = async () => {
@@ -64,11 +63,17 @@ const ChallengeComment = () => {
         nickname,
         password,
         comment,
+        id: nanoid(),
       };
 
       await setDoc(commentDocRef, CommentInfor);
       alert('댓글이 등록되었습니다.');
-      setCount(count + 1);
+
+      // 카운트 값을 업데이트
+      const updatedCount = count + 1;
+      await updateCountInFirestore(updatedCount);
+      setCount(updatedCount);
+
       setNickname('');
       setPassword('');
       setComment('');
@@ -101,7 +106,12 @@ const ChallengeComment = () => {
 
       if (passwordMatched) {
         alert('댓글이 삭제되었습니다.');
-        setCount(count - 1);
+
+        // 카운트 값을 업데이트
+        const updatedCount = count - 1;
+        await updateCountInFirestore(updatedCount);
+        setCount(updatedCount);
+
         fetchComments(); // 댓글 목록을 다시 불러옴
       } else {
         alert('비밀번호가 일치하지 않습니다.');
@@ -111,80 +121,30 @@ const ChallengeComment = () => {
     }
   };
 
-  //   // 댓글 수정
-  //   const handleEditButton = async (id, passwordToEdit) => {
-  //     try {
-  //       const commentCollection = collection(db, 'comments');
-  //       const querySnapshot = await getDocs(commentCollection);
+  // 카운트 값을 Firestore에서 읽어오는 함수
+  const getCountFromFirestore = async () => {
+    const countDocRef = doc(db, 'counts', 'countDocument');
+    const countDocSnap = await getDoc(countDocRef);
+    if (countDocSnap.exists()) {
+      return countDocSnap.data().count;
+    } else {
+      return 0; // 기본값 설정
+    }
+  };
 
-  //       let commentDataToEdit = null; // 수정할 댓글 데이터 저장
+  // 카운트 값을 Firestore에 업데이트하는 함수
+  const updateCountInFirestore = async (newCount) => {
+    const countDocRef = doc(db, 'counts', 'countDocument');
+    await setDoc(countDocRef, { count: newCount });
+  };
 
-  //       querySnapshot.forEach((doc) => {
-  //         const commentData = doc.data();
-
-  //         // 선택된 댓글의 id와 일치하는 댓글을 찾음
-  //         if (id === commentData.id) {
-  //           // 비밀번호가 일치할 경우에만 수정 가능
-  //           if (commentData.password === passwordToEdit) {
-  //             commentDataToEdit = commentData;
-  //           }
-  //         }
-  //       });
-
-  //       // 수정할 댓글 데이터가 있을 때만 수정 모드로 전환
-  //       if (commentDataToEdit) {
-  //         setUpdateComment(commentDataToEdit.comment);
-  //         setEditMode(id); // 수정 모드로 전환
-  //       } else {
-  //         alert('비밀번호가 일치하지 않거나 해당 댓글이 존재하지 않습니다.');
-  //       }
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   };
-
-  // 수정하기 버튼을 클릭하면 수정 모드가 해제되고 수정 내용이 파이어스토어에 저장
-  //   const handleEditSubmit = async (id) => {
-  //     try {
-  //       if (!updateComment) {
-  //         alert('수정할 내용을 입력하세요.');
-  //         return;
-  //       }
-
-  //       const commentCollection = collection(db, 'comments');
-  //       const querySnapshot = await getDocs(commentCollection);
-  //       let commentExists = false; // 해당 댓글이 존재하는지 여부
-
-  //       querySnapshot.forEach((doc) => {
-  //         const commentData = doc.data();
-
-  //         // 선택된 댓글의 id와 일치하는 댓글을 찾음
-  //         if (id === commentData.id) {
-  //           commentExists = true;
-  //         }
-  //       });
-
-  //       if (!commentExists) {
-  //         alert('해당 댓글이 존재하지 않습니다.');
-  //         return;
-  //       }
-
-  //       const commentRef = doc(commentCollection, id);
-
-  //       const updatedData = {
-  //         comment: updateComment,
-  //       };
-
-  //       await updateDoc(commentRef, updatedData);
-  //       alert('댓글이 수정되었습니다.');
-  //       fetchComments();
-  //       // 수정 모드 해제
-  //       setEditMode(null);
-  //       setUpdateComment(''); // 수정 완료 후 상태 초기화
-  //     } catch (error) {
-  //       console.log(error.message);
-  //     }
-  //   };
+  useEffect(() => {
+    const fetchCount = async () => {
+      const count = await getCountFromFirestore();
+      setCount(count);
+    };
+    fetchCount();
+  }, []);
 
   return (
     <>
@@ -217,20 +177,9 @@ const ChallengeComment = () => {
       </form>
       {comments.map((commentData, index) => (
         <CommentBox key={index}>
-          {/* {!editMode || editMode !== commentData.id ? ( */}
           <>
             <p>닉네임: {commentData.nickname}</p>
             <p>댓글: {commentData.comment}</p>
-            {/* <button
-              onClick={() => {
-                const password = prompt('비밀번호를 입력하세요.');
-                if (password !== null) {
-                  handleEditButton(commentData.id, password);
-                }
-              }}
-            >
-              수정
-            </button> */}
             <button
               onClick={() => {
                 const password = prompt('비밀번호를 입력하세요.');
@@ -242,20 +191,6 @@ const ChallengeComment = () => {
               삭제
             </button>
           </>
-          {/* //   ) : ( */}
-          {/* // <>
-            //   <CommentInput */}
-          {/* //     type="text"
-            //     value={updateComment}
-            //     onChange={(e) => { */}
-          {/* //       setUpdateComment(e.target.value);
-            //     }}
-            //   />
-            //   <button onClick={() => handleEditSubmit(commentData.id)}>
-            //     수정하기
-            //   </button>
-            // </> */}
-          {/* )} */}
         </CommentBox>
       ))}
     </>
