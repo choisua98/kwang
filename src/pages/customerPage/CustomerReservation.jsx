f10013a7986d3ca67d06f8e8e2d92785238e0941
Hyung-Jung Son <born2ocode@gmail.com>
2023-09-06 00:15:47 +0900

    Feat / 테마 수정(admin하위페이지 - atom으로 초기화 #4

--- src/pages/customerPage/CustomerReservation.jsx
+++ src/pages/customerPage/CustomerReservation.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReservationService from '../../components/customerSide/options/customerBlocks/ReservationService';
+import { useAtom } from 'jotai';
+import { backgroundImageAtom, themeAtom } from '../../atoms/Atom';
 
 const CustomerReservation = () => {
+  const [theme, setTheme] = useAtom(themeAtom);
+  // 배경 이미지
+  const [backgroundImage, setBackgroundImage] = useAtom(backgroundImageAtom);
+
+  useEffect(() => {
+    setTheme('light');
+    setBackgroundImage(null);
+  }, []);
+
   return <ReservationService />;
 };
 
