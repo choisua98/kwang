04e4dcbbfcfb8a16c37ca3aba61de89731cee829
YongA <ji5919@naver.com>
2023-09-06 00:46:19 +0900

    Style: Customer Options 수정 #95

--- src/components/customerSide/options/CustomerBlocks.style.js
+++ src/components/customerSide/options/CustomerBlocks.style.js
@@ -35,6 +35,24 @@ export const C = {
       margin-bottom: 20px;
     }
 
+    h3 {
+      font-size: 20px;
+      text-align: center;
+      margin: 30px 10px 10px 10px;
+    }
+    h4 {
+      text-align: center;
+      margin-bottom: 30px;
+    }
+
+    img {
+      width: 100%;
+      height: 200px;
+      object-fit: cover; // 이미지가 잘리지 않도록 설정
+      background-color: #d6d6d6;
+      margin: 5px 0px;
+    }
+
     ::placeholder {
       color: #999;
       padding: 5px 7px;
