d3b0234daa5fdb0afcaef4991f21cbafe92bbf94
Hyung-Jung Son <born2ocode@gmail.com>
2023-09-05 20:13:41 +0900

    Feat / 테마 수정 중 #4

--- src/pages/kwangPage/Home.jsx
+++ src/pages/kwangPage/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { H } from './kwangPage.styles';
 import MainBanner from '../../components/Home/MainBanner';
 import Intro from '../../components/Home/Intro';
 import NewCreator from '../../components/Home/NewCreator';
+import { backgroundImageAtom, themeAtom } from '../../atoms/Atom';
+import { useAtom } from 'jotai';
 
 const Home = () => {
+  const [theme, setTheme] = useAtom(themeAtom);
+  // 배경 이미지
+  const [backgroundImage, setBackgroundImage] = useAtom(backgroundImageAtom);
+
+  // onAuthStateChanged 사용
+  useEffect(() => {
+    setTheme('light');
+    setBackgroundImage(null);
+  }, []);
+
   return (
     <H.BodyWrapper>
       <MainBanner />
