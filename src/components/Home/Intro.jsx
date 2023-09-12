dece41687bb4d34c22587b8c5d5d21548bd2a9dd
sua <choisua98@gmail.com>
2023-09-12 15:47:17 +0900

    Feat/메인 시작하기 버튼 로그인 상태일 때 마이홈으로 가기로 바꾸기#5

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import IntroBanner from '../../assets/images/customer/home/banner/intro/intro-banner.png';
-
+import { auth } from '../../firebase/firebaseConfig';
 const Intro = () => {
   const navigate = useNavigate();
+  const user = auth.currentUser;
+  const userUid = user?.uid;
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // 로그인 상태를 저장할 상태 변수
+  // Firebase 인증 상태가 변경될 때마다 호출되는 이벤트 핸들러 등록
+  useEffect(() => {
+    if (user) {
+      // 사용자가 로그인한 경우
+      setIsAuthenticated(true);
+    } else {
+      // 사용자가 로그아웃한 경우
+      setIsAuthenticated(false);
+    }
+    console.log(userUid);
+  }, []);
+
+  // 버튼 텍스트 설정 함수
+  const getButtonText = () => {
+    if (isAuthenticated) {
+      return '마이홈으로 가기';
+    } else {
+      return '시작하기';
+    }
+  };
   return (
     <>
       <img src={IntroBanner} alt="" style={{ width: '100%' }} />
@@ -25,7 +48,14 @@ const Intro = () => {
           나만의 링크 만들기
         </p>
         <button
-          onClick={() => navigate('/login')}
+          onClick={() => {
+            if (isAuthenticated) {
+              // 로그인된 상태에서는 마이홈으로 이동하도록 설정
+              navigate(`/${userUid}`);
+            } else {
+              navigate('/login');
+            }
+          }}
           style={{
             marginTop: '20px',
             width: '129px',
@@ -39,7 +69,7 @@ const Intro = () => {
             boxShadow: '2px 2px 4px 0 rgba(0, 0, 0, 0.25)',
           }}
         >
-          시작하기
+          {getButtonText()}
         </button>
         <address
           style={{
