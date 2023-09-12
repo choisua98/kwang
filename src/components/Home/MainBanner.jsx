64ebbca0ccbc37ea1a0783c8f5482a023833c890
Hyung-Jung Son <born2ocode@gmail.com>
2023-09-12 10:19:58 +0900

    Style / Home.jsx 관련 스타일 컴포넌트 분리 및 불필요파일 삭제 #4

--- src/components/Home/MainBanner.jsx
+++ src/components/Home/MainBanner.jsx
@@ -8,16 +8,13 @@ import {
   Autoplay,
 } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { M } from './MainBanner.styles';
+import { M } from './Home.styles';
 import PromoBanner1 from '../../assets/images/customer/home/banner/promo/promo-banner-1.png';
 import PromoBanner2 from '../../assets/images/customer/home/banner/promo/promo-banner-2.png';
 
 const MainBanner = () => {
   return (
-    <div style={{ padding: '0 20px' }}>
+    <M.MainBannerContainer>
       <Swiper
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         pagination={{ clickable: true }}
@@ -26,14 +23,12 @@ const MainBanner = () => {
           disableOnInteraction: false,
         }}
         loop={true} // 무한 반복 활성화
-        style={{ margin: '25px auto 0', width: '100%' }}
       >
         <SwiperSlide>
           <Link to="#">
             <img
               src={PromoBanner1}
-              alt="Promo Banner 1"
-              style={{ width: '100%' }}
+              alt="크리에이터를 위한 단 하나의 링크, 크왕! 1"
             />
           </Link>
         </SwiperSlide>
@@ -41,13 +36,12 @@ const MainBanner = () => {
           <Link to="#">
             <img
               src={PromoBanner2}
-              alt="Promo Banner 2"
-              style={{ width: '100%' }}
+              alt="크리에이터를 위한 단 하나의 링크, 크왕! 2"
             />
           </Link>
         </SwiperSlide>
       </Swiper>
-    </div>
+    </M.MainBannerContainer>
   );
 };
 
