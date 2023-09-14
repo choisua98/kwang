284da90dc78797a89d5082d4ba76d3c97f49160e
leesoyul <tndus1303@gmail.com>
2023-09-14 17:29:04 +0900

    Fix / UT 피드백 반영, 페이지 uid에 따른 링크 데이터가 남아있는 현상 테스트 2 #63

--- src/components/customerSide/myhome/linkService/LinkService.jsx
+++ src/components/customerSide/myhome/linkService/LinkService.jsx
@@ -7,19 +7,18 @@ import { L } from '../../../adminSide/myadmin/links/Links.styles';
 
 const LinkService = () => {
   const { uid } = useParams();
-
   const [linkDataArray, setLinkDataArray] = useState([]); // 여러 문서 데이터를 저장할 배열
 
   useEffect(() => {
     setLinkDataArray([]); // uid가 변경될 때 담고있는 데이터 초기화
     const fetchData = async () => {
       try {
-        console.log(uid); // uid값 확인
         const q = query(
           collection(db, 'links'),
           where('uid', '==', uid),
           orderBy('createdAt'),
         );
+
         const querySnapshot = await getDocs(q);
 
         const newDataArray = []; // 3개의 문서 데이터를 임시로 담을 배열
@@ -27,10 +26,12 @@ const LinkService = () => {
         // forEach로 uid가 일치하는 문서 데이터 3개를 돌아가며 데이터 추출
         querySnapshot.forEach((doc) => {
           const linkData = doc.data();
-          console.log('Link data', linkData); // 링크 데이터 확인
           newDataArray.push(linkData);
-          setLinkDataArray(newDataArray); // 추출된 문서 데이터를 한 번에 업데이트
         });
+
+        if (newDataArray.length > 0) {
+          setLinkDataArray(newDataArray); // 추출된 문서 데이터를 한 번에 업데이트
+        }
       } catch (error) {
         message.error('에러 발생:', error);
       }
