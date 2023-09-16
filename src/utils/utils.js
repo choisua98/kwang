dca07cdbeb1cd1734f23234b979b659b50b1f10e
YongA <ji5919@naver.com>
2023-09-16 20:38:34 +0900

    Added comments for functions in utils.js #186

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,8 +1,10 @@
+// 수정, 저장 모달 닫기 함수
 export const handleCloseModal = (setModalVisible, navigate) => {
   setModalVisible(false);
   navigate(-1);
 };
 
+// 삭제 모달 닫기 함수
 export const handleCloseDeleteModal = (setDeleteModalVisible, navigate) => {
   setDeleteModalVisible(false);
   navigate(-1);
